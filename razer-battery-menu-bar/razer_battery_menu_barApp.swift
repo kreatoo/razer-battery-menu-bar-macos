1701e8bda029021ac72e83c91b39f980a6faebfb
Alex Perathoner <alex.perathoner@gmail.com>
2024-11-26 11:05:21 +0100

    Initial Commit

---
+++ razer-battery-menu-bar/razer_battery_menu_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  razer_battery_menu_barApp.swift
+//  razer-battery-menu-bar
+//
+//  Created by Alex Perathoner on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct razer_battery_menu_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
